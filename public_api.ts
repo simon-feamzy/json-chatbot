21091b09405ef89dc9718ffdd713a8f5f4bb1008
GitLab CI/CD <simon.guerard@feamzy.com>
2021-05-29 11:31:41 +0200

    init library

---
+++ public_api.ts
@@ -0,0 +1,2 @@
+export * from "./src/app/modules/chatbot/chatbot.module"
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ChatbotModule} from "./modules/chatbot/chatbot.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChatbotModule
   ],
   providers: [],
   bootstrap: [AppComponent]
