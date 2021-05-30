32b00876bc9dddada19bce1e9bf12316d4b76d96
GitLab CI/CD <simon.guerard@feamzy.com>
2021-05-30 10:30:35 +0200

    add input answer type

--- src/app/modules/chatbot/ionic-chatbot/ionic-chatbot.component.ts
+++ src/app/modules/chatbot/ionic-chatbot/ionic-chatbot.component.ts
@@ -12,6 +12,7 @@ export class IonicChatbotComponent implements OnInit {
   @Input() botName: string = "bot";
   @Input() userName: string = "you";
   @Input() jsonFile: string = "";
+  @Input() mapResult: {} = {};
 
   messages: ChatMessage[] = [];
   colorCode?: string
@@ -26,32 +27,43 @@ export class IonicChatbotComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    debugger
     this.utilsService.parseJson(this.jsonFile).subscribe((rootElt: Step) => {
-      debugger
       this.currentMsg = rootElt;
       this.displayStep();
     });
   }
 
-  displayStep(){
+  displayStep() {
     const msg = new ChatMessage(MessageType.MSG_RES, this.botName, this.currentMsg?.text, UtilsService.getEpoch());
     this.messages.push(msg);
     if (this.currentMsg?.answerType == AnswerType.BUTTON) {
       this.answerButton = true;
       this.currentAnswers = this.currentMsg.answers;
+    } else if (this.currentMsg?.answerType == AnswerType.INPUT) {
+      this.answerInput = true;
+      this.currentAnswers = this.currentMsg.answers;
     }
   }
 
-  sendMessage(answer: Answer) {
-    console.debug("IonicChatbotComponent.sendMessage : "+answer);
-    const msg = new ChatMessage(MessageType.MSG_REQ, this.userName, answer.text, UtilsService.getEpoch());
-    setTimeout(function () {}, 1000);
+  sendMessage(type: AnswerType, answer: Answer) {
+    console.debug("IonicChatbotComponent.sendMessage : " + answer);
+    let msg: ChatMessage;
+    if (type == AnswerType.INPUT) {
+      msg = new ChatMessage(MessageType.MSG_REQ, this.userName, this.content, UtilsService.getEpoch());
+    } else {//    if (type == AnswerType.BUTTON) {
+      msg = new ChatMessage(MessageType.MSG_REQ, this.userName, answer.text, UtilsService.getEpoch());
+    }
+    setTimeout(function () {
+    }, 1000);
+    // @ts-ignore
+    this.mapResult[answer.action] = this.content;
     this.messages.push(msg);
     this.currentMsg = this.utilsService.getNextStep(answer.action);
     this.answerButton = false;
     this.currentAnswers = [];
-      setTimeout(function () {}, 1000);
+    setTimeout(function () {
+    }, 1000);
     this.displayStep();
   }
+
 }
